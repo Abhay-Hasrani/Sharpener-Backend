d2a85f297881c4c7802e7102d70b173ad1c96b18
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-08-28 22:40:30 +0530

    sent first custom http request worked with buffer and file system fs

--- firstServer.js
+++ firstServer.js
@@ -1,12 +1,47 @@
 const http = require("http");
+const fs = require("fs");
+
 const server = http.createServer((req, res) => {
-  console.log("Abhay in the server");
-  let text="Welcome to my Node Js project";
-  console.log(req.url);
-  if(req.url==="/home") text="Welcome Home";
-  if(req.url==="/about") text="Welcome to About Us Page";
-  res.setHeader('Content-Type','text/html');
-  res.write(`<html><body><h1>${text}</title></h1></html>`);
-  res.end();
+  //   console.log("Abhay in the server");
+  let text = "Welcome to my Node Js project";
+  //   console.log(req.url);
+  const url = req.url;
+  const method = req.method;
+  console.log(url, method);
+  if (req.url === "/home") text = "Welcome Home";
+  if (req.url === "/about") text = "Welcome to About Us Page";
+  if (url === "/") {
+    const message = fs.readFileSync("message.txt");
+    res.setHeader("Content-Type", "text/html");
+    res.write(`
+  <html>
+  <body>
+  <h1>${text}</h1>
+  <h3>${message}</h3>
+  <form action="/message" method="POST">
+  <input type="text" name="message">
+  <button type="submit">Send</button>
+  </form>
+  </body>
+  </html>`);
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const data = Buffer.concat(body).toString();
+      const message = data.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        //   console.log(message);
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
 });
 server.listen(4000);
