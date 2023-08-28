1f67324552140a5e494fcb82a7a686bd51384237
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-08-28 20:57:55 +0530

    used req.url ,res.write,res.setHeader and sent html response

--- firstServer.js
+++ firstServer.js
@@ -1,5 +1,12 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   console.log("Abhay in the server");
+  let text="Welcome to my Node Js project";
+  console.log(req.url);
+  if(req.url==="/home") text="Welcome Home";
+  if(req.url==="/about") text="Welcome to About Us Page";
+  res.setHeader('Content-Type','text/html');
+  res.write(`<html><body><h1>${text}</title></h1></html>`);
+  res.end();
 });
 server.listen(4000);
