6a8b5ce2375c66818c2432d935eea40b59c9fe83
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-08-29 19:19:27 +0530

    added express and used middleware in firstSever

--- First Server/firstServer.js
+++ First Server/firstServer.js
@@ -1,8 +1,23 @@
-const http = require("http");
+// const http = require("http");
+
+const express = require("express");
 
 //custom imports
-const routes = require('./routes'); 
+// const routes = require('./routes'); 
 
-const server = http.createServer(routes.handler);
-server.listen(4000); 
-  
+const app = express();
+
+//using middlewares
+app.use((req,res,next)=>{
+    console.log("Middleware 1");
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log("Middleware 2");
+    res.send("<h1>Hello this is express JS</h1>");
+});
+
+// const server = http.createServer(routes.handler); //doesnt need this as app.listens does same bts
+// server.listen(4000);
+app.listen(4000);
