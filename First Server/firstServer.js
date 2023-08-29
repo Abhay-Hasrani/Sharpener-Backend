e23644dd94b91c82e14bee5875e5dbbea4acca5f
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-08-29 19:47:09 +0530

    used middle ware and body parser to parse a request by form

--- First Server/firstServer.js
+++ First Server/firstServer.js
@@ -1,21 +1,24 @@
 // const http = require("http");
 
 const express = require("express");
-
+const bodyParser = require("body-parser");
 //custom imports
-// const routes = require('./routes'); 
+// const routes = require('./routes');
 
 const app = express();
 
 //using middlewares
-app.use((req,res,next)=>{
-    console.log("Middleware 1");
-    next();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input name="title" type="text"><input name="size" type="number"><button type="submit">Send</button></form>'
+  );
+  res.redirect("/product");
 });
 
-app.use((req,res,next)=>{
-    console.log("Middleware 2");
-    res.send("<h1>Hello this is express JS</h1>");
+app.post("/product",(req, res, next) => {
+  console.log(req.body);
+  res.send("<h1>Hello this is express JS</h1>");
 });
 
 // const server = http.createServer(routes.handler); //doesnt need this as app.listens does same bts
