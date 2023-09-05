1e3f0e3153e6fe5b7eacd2c77c5f4cb9999a39be
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-09-05 21:52:57 +0530

    secured exposed password in database using bcrypt hashing

--- Expense Tracker Full Stack/server/controllers/auth.js
+++ Expense Tracker Full Stack/server/controllers/auth.js
@@ -1,21 +1,33 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res, next) => {
   const userdata = req.body;
   const username = userdata.username;
   const email = userdata.email;
   const password = userdata.password;
-  try {
-    // console.log(userdata);
-    const newUser = await User.create({ username, email, password });
-    res.statusMessage = "User Registered and account created successfully";
-    res.status(200).json(newUser);
-  } catch (err) {
-    console.log(err);
-    res.statusMessage =
-      "E-mail is already exists. Please LogIn or Use different E-mail";
-    res.status(400).json(err);
-  }
+  /*bcrypt uses salt rounds for number of times randomizing our password and
+     generates a hash password than can be checked using compare*/
+  const saltRounds = 10;
+  bcrypt.hash(password, saltRounds, async (err, hash) => {
+    try {
+      if (err) throw new Error("Error while generating hash");
+      else if (hash) {
+        const newUser = await User.create({
+          username,
+          email,
+          password: hash,
+        });
+        res.statusMessage = "User Registered and account created successfully";
+        res.status(200).json(newUser);
+      }
+    } catch (err) {
+      console.log("email esists error");
+      res.statusMessage =
+        "E-mail is already exists. Please LogIn or Use different E-mail";
+      res.status(400).json(err);
+    }
+  });
 };
 
 exports.postUserLogin = async (req, res, next) => {
@@ -23,19 +35,22 @@ exports.postUserLogin = async (req, res, next) => {
   const email = userdata.email;
   const password = userdata.password;
   try {
-    // console.log(userdata);
     const user = await User.findOne({
       where: {
         email: email,
       },
     });
-    if (user.password != password) {
-      res.statusMessage = "Wrong Password!!!";
-      res.status(400).json(new Error());
-    } else {
-      res.statusMessage = "User Logged In successfully";
-      res.json(user);
-    }
+    bcrypt.compare(password, user.password, (err, result) => {
+      // result is boolean
+      if (err) throw new Error("Error comparing passwrods in log in");
+      if (!result) {
+        res.statusMessage = "Wrong Password!!!";
+        res.status(400).json(new Error());
+      } else {
+        res.statusMessage = "User Logged In successfully";
+        res.json(user);
+      }
+    });
   } catch (err) {
     console.log(err);
     res.statusMessage = "E-mail doesn't exists. Please check again!";
