1fe7092475168980438410417b7a28bb166b963b
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-09-21 16:19:20 +0530

    added type caster updated file structure and separated JS and TS

--- node-typescript-REST/dist/routers/todos.js
+++ node-typescript-REST/dist/routers/todos.js
@@ -7,20 +7,23 @@ router.get("/", (req, res, next) => {
     res.status(200).json({ todos });
 });
 router.post("/add-todo", (req, res, next) => {
-    const newTodo = { id: new Date().toISOString(), text: req.body.text };
+    const body = req.body;
+    const newTodo = { id: new Date().toISOString(), text: body.text };
     //   res.status(200).json({ todos });
     console.log(newTodo);
     todos.push(newTodo);
     res.status(200).json({ newTodo });
 });
 router.post("/delete-todo", (req, res, next) => {
-    const id = req.body.id;
+    const body = req.body;
+    const id = body.id;
     todos = todos.filter((item) => item.id !== id);
     res.status(200).json({ status: "Success", todos });
 });
 router.post("/edit-todo", (req, res, next) => {
-    const id = req.body.id;
-    const newText = req.body.text;
+    const body = req.body;
+    const id = body.id;
+    const newText = body.text;
     const ind = todos.findIndex((item) => item.id === id);
     todos[ind].text = newText;
     res.status(200).json({ todos });
