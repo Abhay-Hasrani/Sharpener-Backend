37b6f13641c9a456e21b620ec2e5d6d07ec58772
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-10-04 13:15:21 +0530

    added CRUD operations for product shop and admin

--- e-commerce Mongodb/models/product.js
+++ e-commerce Mongodb/models/product.js
@@ -1,19 +1,69 @@
+const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, price, imageUrl, description) {
+  constructor(title, price, imageUrl, description, id) {
     this.title = title;
     this.price = price;
     this.imageUrl = imageUrl;
     this.description = description;
+    this._id = new mongodb.ObjectId(id);
   }
 
   save() {
     const db = getDb();
-    const collection = db.collection("shop");
-    return collection
-      .insertOne(this)
+    const productCollection = db.collection("shop");
+    let dbOp;
+    if (this._id) {
+      dbOp = productCollection.updateOne(
+        {
+          _id: this._id,
+        },
+        { $set: this }
+      );
+    } else {
+      dbOp = productCollection.insertOne(this);
+    }
+    return dbOp
       .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  }
+
+  static delete(id) {
+    const db = getDb();
+    const productCollection = db.collection("shop");
+    return productCollection
+      .deleteOne({ _id: new mongodb.ObjectId(id) })
+      .then((result) => {
+        console.log(result);
+        return result;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    const productCollection = db.collection("shop");
+    return productCollection
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static findById(id) {
+    const db = getDb();
+    const productCollection = db.collection("shop");
+    return productCollection
+      .find({ _id: new mongodb.ObjectId(id) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
+      })
       .catch((err) => console.log(err));
   }
 }
