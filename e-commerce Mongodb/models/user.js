4ff5791e9491f9a2b6a8b524103b8baa017b7792
Abhay-Hasrani <abhayhasrani@gmail.com>
2023-10-04 14:14:56 +0530

    added User model

--- e-commerce Mongodb/models/user.js
+++ e-commerce Mongodb/models/user.js
@@ -1,16 +1,33 @@
-const Sequelize = require('sequelize');
-
-const sequelize = require('../util/database');
-
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+const ObjectId = require("mongodb").ObjectId;
+const getDb = require("../util/database").getDb;
+class User {
+  constructor(name, email, id) {
+    this.name = name;
+    this.email = email;
+    this._id = id ? new ObjectId(id) : null;
+  }
+  save() {
+    const db = getDb();
+    const userCollection = db.collection("users");
+    return userCollection
+      .insertOne(this)
+      .then((product) => {
+        console.log(product);
+        return product;
+      })
+      .catch((err) => console.log(err));
+  }
+  static findUserById(id) {
+    const db = getDb();
+    const userCollection = db.collection("users");
+    return userCollection
+      .findOne({ _id: new ObjectId(id) })
+      .then((product) => {
+        console.log(product);
+        return product;
+      })
+      .catch((err) => console.log(err));
+  }
+}
 
 module.exports = User;
